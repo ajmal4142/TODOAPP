f24270b51b2513e59d92b99f239f3998eb6e74a1
ajmal <aju6697@gmail.com>
2022-09-20 16:52:10 +0530

    Delete Stage is completed

--- src/App.js
+++ src/App.js
@@ -1,69 +1,91 @@
-import React from 'react'
-import './App.css'
-import {useState} from 'react'
-import Delete from './Delete/Delete'
-import WorkDone from './WorkDone/Done'
-function App(){
-  const [todo,setTodo] = useState('');
-  const [todos,setTodos] =useState([]);
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import WorkDone from "./WorkDone/Done";
+function App() {
+  const Delete = (obj) => {
+    const dels = todos.filter((del) => del !== obj);
+    console.log(dels);
+    setTodos(dels);
+  };
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div className='Mainbody'>
+    <div className="Mainbody">
       <h1 className="mainhead">ToDo List</h1>
-      <hr className='border'></hr>
-      <h3 className="subheading"> Memmorize Your Daily Activities  </h3>
-      <h1 className='Design'>-------------**------------</h1>
+      <hr className="border"></hr>
+      <h3 className="subheading"> Memmorize Your Daily Activities </h3>
+
       <div className="contents">
-      <div className="mainProgress">
-      <div className="texting">
-      <input value={todo} onChange={(e)=>{setTodo(e.target.value)}} type="text" placeholder='Type here....' className='progessText'/>
-      
-      <i onClick={()=>setTodos([...todos,{id:Date.now(),text:todo,status:false,is_delete:false}])} className="fa-thin fa-plus"></i>
-      </div>
-      
-    <div className="Main-display">
-    {todos.map((obj,intex)=>{
-    return(<div key={intex} className="progressDisplay">
-      <div className="type">
-      <p className='text'>{obj.text}</p>
-      </div>
-      
-      <input className='check' onChange={(e)=>{console.log(e.target.checked)
-      console.log(todos);
-        setTodos(todos.filter((obj2)=>{
-        if(obj2.id===obj.id){
-          obj2.status=e.target.checked;
-          
-        }
-        return obj2;
+        <div className="mainProgress">
+          <div className="texting">
+            <input
+              value={todo}
+              onChange={(e) => {
+                setTodo(e.target.value);
+              }}
+              type="text"
+              placeholder="Type here...."
+              className="progessText"
+            />
 
-      }))}
-     
-    } value={obj.status} type="checkbox" />
-        <i className='fa-solid fa-trash' onClick={()=>{console.log(todos)
-  console.log(todos);
-    setTodos(todos.filter((del2)=>{
-    if(del2.id===obj.id){
-      del2.is_delete=true;
-      
-    }
-    return del2;
+            <i
+              onClick={() =>
+                setTodos([
+                  ...todos,
+                  {
+                    id: Date.now(),
+                    text: todo,
+                    status: false,
+                  },
+                ])
+              }
+              className="fa-thin fa-plus"
+            ></i>
+          </div>
 
-  }))}
- 
-} value={obj.is_delete}  ></i>
-      <i className="fa-sharp fa-solid fa-pen-to-square"></i>
-      
-      </div>)})}
-      
+          <div className="Main-display">
+            {todos.map((obj, intex) => {
+              return (
+                <div key={intex} className="progressDisplay">
+                  <div className="type">
+                    <p className="text">{obj.text}</p>
+                  </div>
+
+                  <input
+                    className="check"
+                    onChange={(e) => {
+                      console.log(e.target.checked);
+                      console.log(todos);
+                      setTodos(
+                        todos.filter((obj2) => {
+                          if (obj2.id === obj.id) {
+                            obj2.status = e.target.checked;
+                          }
+                          return obj2;
+                        })
+                      );
+                    }}
+                    value={obj.status}
+                    type="checkbox"
+                  />
+                  <i
+                    className="fa-solid fa-trash"
+                    onClick={() => {
+                      Delete(obj);
+                      console.log(obj);
+                    }}
+                  ></i>
+                  <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <WorkDone setTodos={setTodos} todos={todos} />
       </div>
     </div>
-      <WorkDone todos={todos}/>
-      <Delete todos={todos}/>
-      </div>
-    </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/WorkDone/Done.jsx
+++ src/WorkDone/Done.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import './Done.css'
+import React from "react";
+import "./Done.css";
 const Done = (props) => {
+  const { todos, setTodos } = props;
+  console.log(todos);
+  const Deleted = (obj) => {
+    const dels = todos.filter((del) => del !== obj);
+    console.log(dels);
+    setTodos(dels);
+  };
   return (
-    
-      <div className="mainDone">
-       
-        <div className="captionbox">
-          <h3 className="capdone">DONE</h3>
-        </div>
-        
-        {props.todos.map((obj,intex)=>{
-          if(obj.status){
-            return (<div key={intex} className="donebox">
-          <div className="type">
-             <p className='text'>{obj.text}</p>
-          </div>
-          <i className='fa-solid fa-trash'></i>
-        </div>)
-          }
-         return null;
-        })}
-        
-        
+    <div className="mainDone">
+      <div className="captionbox">
+        <h3 className="capdone">DONE</h3>
       </div>
-    
-  )
-}
+      <div className="Main-displa">
+        <div className="dis">
+          {todos.map((obj, intex) => {
+            console.log(todos);
+            if (obj.status) {
+              return (
+                <div key={intex} className="donebox">
+                  <div className="type">
+                    <p className="text">{obj.text}</p>
+                  </div>
+                  <i
+                    onClick={() => {
+                      Deleted(obj);
+                      console.log(obj);
+                    }}
+                    className="fa-solid fa-trash"
+                  ></i>
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Done
+export default Done;
